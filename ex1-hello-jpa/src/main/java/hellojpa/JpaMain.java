62f5150f9f0bd80911cbdf4ee921b7501c861ce5
sdfgx123 <sdfgx123@naver.com>
2022-12-01 21:47:27 +0900

    221201

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,21 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        Member member = new Member();
+
+        member.setId(1L);
+        member.setName("HelloA");
+
+        em.persist(member);
+        em.close();
+        emf.close();
+    }
+}
