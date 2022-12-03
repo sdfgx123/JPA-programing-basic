42f1328bcfc2cd3443d878382470b20a9072221e
sdfgx123 <sdfgx123@naver.com>
2022-12-03 16:46:14 +0900

    20221203

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -57,14 +57,21 @@ public class JpaMain {
             em.flush();
             System.out.println("---------------------");*/
 
-            Member member = em.find(Member.class, 150L);
+            /*Member member = em.find(Member.class, 150L);
             member.setName("AAAAA");
             // detach 메서드를 통해 member 객체를 영속성 컨텍스트에서 제외함 >  더 이상 commit이 일어나지 않음
             //em.detach(member);
             // clear 하면 영속성 컨텍스트를 통으로 다 날려버림, 즉 1차 캐시를 초기화함
             em.clear();
             Member member2 = em.find(Member.class, 150L);
-            System.out.println("----------------");
+            System.out.println("----------------");*/
+
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
+
+            em.persist(member);
 
             /*
             엔티티 매니저 팩토리는 하나만 생성 후 앱 전체에서 공유
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,22 +1,42 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
-
-    // PK
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob // varchar를 뛰어넘는 큰 컨텐츠를 저장할 때 사용
+    private String description;
 
     public Member() {
+
     }
 
-    public Member(Long id, String name) {
+    public Member(Long id, String username, Integer age, RoleType roleType, Date createdDate, Date lastModifiedDate, String description) {
         this.id = id;
-        this.name = name;
+        this.username = username;
+        this.age = age;
+        this.roleType = roleType;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.description = description;
     }
 
     public Long getId() {
@@ -27,11 +47,51 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
