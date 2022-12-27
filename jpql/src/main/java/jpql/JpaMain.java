9f9e5ca8f3709a667ec0886822d604dbe3ddead5
sdfgx123 <sdfgx123@naver.com>
2022-12-27 20:37:57 +0900

    20221227

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -18,6 +19,18 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+
+            System.out.println("MemberDTO = " + memberDTO.getUsername());
+            System.out.println("MemberDTO = " + memberDTO.getAge());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
